e12dff509103439ce090da89d15b4374a9efb21b
Diullei <diullei@gmail.com>
2014-07-19 20:41:51 -0300

    adding euler problems

---
+++ Euler/haskell/Template.hs
@@ -0,0 +1,13 @@
+import Test.QuickCheck
+import Test.QuickCheck.All
+
+{- ------------- -}
+{- SOLUTION HERE -}
+{- ------------- -}
+
+{- ------------- -}
+{- TEST CASE     -}
+{- ------------- -}
+
+main = do
+    quickCheck (fn args == expected)
