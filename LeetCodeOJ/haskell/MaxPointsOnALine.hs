44df318ed96a63ecc70c1f5f3f5d8438fab455a1
Diullei <diullei@gmail.com>
2014-07-20 18:46:15 -0300

    annotations

--- LeetCodeOJ/haskell/MaxPointsOnALine.hs
+++ LeetCodeOJ/haskell/MaxPointsOnALine.hs
@@ -1,6 +1,11 @@
 import Test.QuickCheck
 import Data.List (delete)
 
+{-
+    Given n points on a 2D plane, find the maximum number of points that lie on the same straight line.
+    https://oj.leetcode.com/problems/max-points-on-a-line/
+-}
+
 {- ------------- -}
 {- SOLUTION HERE -}
 {- ------------- -}
