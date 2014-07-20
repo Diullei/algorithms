09341a4608a18b55253e89d5add4657cd73f1275
Diullei <diullei@gmail.com>
2014-07-19 22:32:36 -0300

    new leetCodeOj problem

---
+++ LeetCodeOJ/haskell/ReverseWordsinAString.hs
@@ -0,0 +1,34 @@
+import           Test.QuickCheck
+
+{-
+    Given an input string, reverse the string word by word.
+
+    For example,
+    Given s = "the sky is blue",
+    return "blue is sky the".
+-}
+
+{- ------------- -}
+{- SOLUTION HERE -}
+{- ------------- -}
+
+split' :: [Char] -> [[Char]]
+split' [] = []
+split' cs = [takeW] ++ split' (drop ((length takeW) + 1) cs)
+    where
+        takeW = (takeWhile (\c -> c /= ' ') cs)
+
+reverse' :: [Char] -> [Char]
+reverse' [] = []
+reverse' sss = take (length sss) (foldl (\a b -> b ++ " " ++ a) "" (split' sss))
+
+{- ------------- -}
+{- TEST CASE     -}
+{- ------------- -}
+
+main :: IO ()
+main = do
+    {- split' -}
+    quickCheck (split' "the sky is blue" == ["the", "sky", "is", "blue"])
+    {- reverse' -}
+    quickCheck (reverse' "the sky is blue" == "blue is sky the")
